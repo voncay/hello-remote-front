8e9360d1dc9fd5e30683266ed9564b125afa01b0
Barbara <barbara@git>
2023-02-06 01:59:25 +0100

    remove limit search row

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -53,98 +53,9 @@ const SearchBar = () => {
             </div>
             {/* End Input Card */}
           </form>
-          <div className="row align-items-center">
-            <div className="col-md-auto mb-3 mb-lg-0">
-              <h6 className="mb-1">Limit search to:</h6>
-            </div>
-            {/* End Col */}
-            <div className="col-md mb-3 mb-lg-0">
-              {/* Check */}
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="jobSearchToCheckbox1"
-                  defaultValue="option1"
-                  defaultChecked=""
-                />
-                <label
-                  className="form-check-label"
-                  htmlFor="jobSearchToCheckbox1"
-                >
-                  Job title
-                </label>
-              </div>
-              {/* End Check */}
-              {/* Check */}
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="jobSearchToCheckbox2"
-                  defaultValue="option2"
-                />
-                <label
-                  className="form-check-label"
-                  htmlFor="jobSearchToCheckbox2"
-                >
-                  Skills
-                </label>
-              </div>
-              {/* End Check */}
-              {/* Check */}
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="jobSearchToCheckbox3"
-                  defaultValue="option3"
-                />
-                <label
-                  className="form-check-label"
-                  htmlFor="jobSearchToCheckbox3"
-                >
-                  Companies
-                </label>
-              </div>
-              {/* End Check */}
-              {/* Check */}
-              <div className="form-check form-check-inline">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="jobSearchToCheckbox4"
-                  defaultValue="option4"
-                />
-                <label
-                  className="form-check-label"
-                  htmlFor="jobSearchToCheckbox4"
-                >
-                  Field of study
-                </label>
-              </div>
-              {/* End Check */}
-            </div>
-            <div className="col-md-auto">
-              {/* Switch */}
-              <div className="form-check form-switch">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  id="remoteOnlySwitch"
-                />
-                <label className="form-check-label" htmlFor="remoteOnlySwitch">
-                  Remote only
-                </label>
-              </div>
-              {/* End Switch */}
-            </div>
-            {/* End Col */}
-          </div>
-          {/* End Row */}
         </div>
       </div>
-      {/* End Hero */}ß
+      {/* End Hero */}
     </>
   );
 };
